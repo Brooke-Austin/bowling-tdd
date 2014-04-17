cf4dcc07f252dc9b1ce3b7274d755deea7968eb9
Daniel Brooks <Daniel_C_Brooks@DellTeam.com>
2014-04-17 15:05:10 -0500

    First day of coding Bowling project

---
+++ Bowling.tests/GameTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Bowling.Tests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void first_roll()
+        {
+            var game = new Game();
+
+            game.AddRoll(7);
+
+            game.GetScore().ShouldBe(7);
+        }
+
+        [Test]
+        public void two_rolls_under10()
+        {
+            var game = new Game();
+
+            game.AddRoll(7);
+            game.AddRoll(2);
+            
+            game.GetScore().ShouldBe(9);
+        }
+
+        [Test]
+        public void my_little_spare()
+        {
+            var game = new Game();
+
+            game.AddRoll(5);
+            game.AddRoll(5);
+       
+            game.AddRoll(4);
+
+            // total pins: 14
+            
+            // first frame raw: 10
+            // second frame raw: 4
+            // first frame spare bonus: 4
+            
+            game.GetScore().ShouldBe(18);
+        }
+    }
+}
---
+++ Bowling/Game.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Bowling
+{
+    public class Game
+    {
+        private readonly List<Frame> _frames = new List<Frame> {new Frame(null)};
+
+        public void AddRoll(int i)
+        {
+            Frame frame = _frames.Last();
+            frame.AddRoll(i);
+            if (frame.IsComplete())
+            {
+                _frames.Add(new Frame(frame));
+            }
+        }
+
+        public int GetScore()
+        {
+            return _frames.Sum(x => x.GetScore());
+        }
+    }
+
+    public class Frame
+    {
+        private readonly Frame _previous;
+        private int _frameScore;
+        private int _rolls;
+
+        public Frame(Frame previous)
+        {
+            _previous = previous;
+        }
+
+        public void AddRoll(int pins)
+        {
+            _frameScore += pins;
+            _rolls++;
+
+            if (_previous != null) 
+                _previous.AcceptBonus(pins);
+        }
+
+        public bool IsComplete()
+        {
+            return _frameScore == 10 || _rolls == 2;
+        }
+
+        public int GetScore()
+        {
+            return _frameScore;
+        }
+
+        private void AcceptBonus(int i)
+        {
+            if (Spare()) 
+                _frameScore += i;
+        }
+
+        private bool Spare()
+        {
+            return _frameScore == 10 && _rolls == 2;
+        }
+    }
+}
